e1c8e2e5a3b4d177924c5178be2f7a42d899d6ec
Viktor <sarinviktor8755@gmail.com>
2025-03-30 10:59:01 +0600

    Automatic commit 30.03.2025 10:59:00,98

--- git_push.bat
+++ git_push.bat
@@ -1,18 +1,7 @@
 @echo off
 setlocal
 
-:: Запрашиваем сообщение коммита
-set /p commit_message="Введите сообщение коммита: "
-
-:: Добавляем все изменения
 git add .
-
-:: Создаем коммит
-git commit -m "%commit_message%"
-
-:: Пушим изменения в удаленный репозиторий (по умолчанию в origin и текущую ветку)
+git commit -m "Automatic commit %date% %time%"
 git push
-
-echo.
-echo Операция завершена!
 pause
